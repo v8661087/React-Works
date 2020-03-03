3302c8abe967c47e60947cf5beda28e6a51b8bae
v8661087 <v8661087@gmail.com>
2020-03-03 20:34:24 +0800

    Updates

---
+++ precache-manifest.184c72dd6d289e0fafc2633a01da5ccd.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc4f099177c508087732",
+    "url": "/React-Works/static/css/main.c33a48f0.chunk.css"
+  },
+  {
+    "revision": "cc4f099177c508087732",
+    "url": "/React-Works/static/js/main.10f5e75f.chunk.js"
+  },
+  {
+    "revision": "71479fc5dd82b67c74e2",
+    "url": "/React-Works/static/js/runtime~main.911d5c7c.js"
+  },
+  {
+    "revision": "550004ea84b08aaa1cc4",
+    "url": "/React-Works/static/js/2.d72a0662.chunk.js"
+  },
+  {
+    "revision": "981a9625d6d19f18cc5c290ccb0ee41b",
+    "url": "/React-Works/static/media/airFlow.981a9625.svg"
+  },
+  {
+    "revision": "881734c06e6bbfd0addb6e1459f25d60",
+    "url": "/React-Works/static/media/rain.881734c0.svg"
+  },
+  {
+    "revision": "ed958a11665374f2c6b0ce938a469a9c",
+    "url": "/React-Works/static/media/refresh.ed958a11.svg"
+  },
+  {
+    "revision": "bffa65e7f30ad0a23468893a58cf1445",
+    "url": "/React-Works/static/media/loading.bffa65e7.svg"
+  },
+  {
+    "revision": "7ca9fe14f0863a8639463718f68ee69f",
+    "url": "/React-Works/static/media/cog.7ca9fe14.svg"
+  },
+  {
+    "revision": "32a450a8eb3d385943af76f3738204b1",
+    "url": "/React-Works/static/media/day-thunderstorm.32a450a8.svg"
+  },
+  {
+    "revision": "19e2ef132632c2a41a547fd987ea4332",
+    "url": "/React-Works/static/media/day-clear.19e2ef13.svg"
+  },
+  {
+    "revision": "296112b168b03996ac80e9c7d324bd80",
+    "url": "/React-Works/static/media/day-cloudy-fog.296112b1.svg"
+  },
+  {
+    "revision": "5c66b1345ba4954b591a9abd473fec8e",
+    "url": "/React-Works/static/media/day-cloudy.5c66b134.svg"
+  },
+  {
+    "revision": "4b1ec37a80fcfeb3339628e1d941bd95",
+    "url": "/React-Works/static/media/day-fog.4b1ec37a.svg"
+  },
+  {
+    "revision": "51c7cd390b88854769659caf903cf233",
+    "url": "/React-Works/static/media/day-partially-clear-with-rain.51c7cd39.svg"
+  },
+  {
+    "revision": "dd8509241b9184ffc6498e27e256d12d",
+    "url": "/React-Works/static/media/day-snowing.dd850924.svg"
+  },
+  {
+    "revision": "5f007c776d1d327f40da690c8014a9d8",
+    "url": "/React-Works/static/media/night-thunderstorm.5f007c77.svg"
+  },
+  {
+    "revision": "a99547e9b50556379a96c369d2cd8a8c",
+    "url": "/React-Works/static/media/night-clear.a99547e9.svg"
+  },
+  {
+    "revision": "c78ef76288a695f6d4d010fd4441b8b7",
+    "url": "/React-Works/static/media/night-cloudy-fog.c78ef762.svg"
+  },
+  {
+    "revision": "3f25314886812bb140ce00c75ce38df7",
+    "url": "/React-Works/static/media/night-cloudy.3f253148.svg"
+  },
+  {
+    "revision": "2477d0a9b2c5d67dc8b806ff6dc30d42",
+    "url": "/React-Works/static/media/night-fog.2477d0a9.svg"
+  },
+  {
+    "revision": "3a37b82a0663f3627389043001c0c190",
+    "url": "/React-Works/static/media/night-partially-clear-with-rain.3a37b82a.svg"
+  },
+  {
+    "revision": "19cb9f62d708528aa8145d69d7d1c713",
+    "url": "/React-Works/static/media/night-snowing.19cb9f62.svg"
+  },
+  {
+    "revision": "aecf7b389a032da4496c7df624149a43",
+    "url": "/React-Works/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React-Works/precache-manifest.aa40e5b35c742306044c23d6659174a1.js"
+  "/React-Works/precache-manifest.184c72dd6d289e0fafc2633a01da5ccd.js"
 );
 
 workbox.clientsClaim();
